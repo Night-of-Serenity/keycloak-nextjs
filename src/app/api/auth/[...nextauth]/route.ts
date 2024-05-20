794702700ccbf1d6814d0a5f231a95d22ef2b54b
Peerapat Udtha(Night) <peerapat.ud@gmail.com>
2024-05-20 11:17:17 +0700

    make reflesh token fn, add session guard and provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,19 +3,19 @@ import NextAuth, { AuthOptions } from "next-auth"; // Import the NextAuth module
 import KeycloakProvider from "next-auth/providers/keycloak";
 
 // api call to refresh the access token
-// function requestRefreshOfAccessToken(token: JWT) {
-//   return fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: new URLSearchParams({
-//       client_id: process.env.KEYCLOAK_CLIENT_ID,
-//       client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
-//       grant_type: "refresh_token",
-//       refresh_token: token.refreshToken!,
-//     }),
-//     method: "POST",
-//     cache: "no-store",
-//   });
-// }
+function requestRefreshOfAccessToken(token: JWT) {
+  return fetch(`${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/token`, {
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_id: process.env.KEYCLOAK_CLIENT_ID,
+      client_secret: process.env.KEYCLOAK_CLIENT_SECRET,
+      grant_type: "refresh_token",
+      refresh_token: token.refreshToken!,
+    }),
+    method: "POST",
+    cache: "no-store",
+  });
+}
 
 export const authOptions: AuthOptions = {
   providers: [
@@ -26,6 +26,57 @@ export const authOptions: AuthOptions = {
       issuer: process.env.KEYCLOAK_ISSUER,
     }),
   ],
+  session: {
+    maxAge: 60 * 30, // reflesh token expires in 30 minutes
+  },
+  callbacks: {
+    // called when the user is logged in
+    async jwt({ token, account }) {
+      // modify the token with the account data
+      if (account) {
+        console.log("token", token);
+        console.log("account", account);
+        token.idToken = account.id_token;
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+        return token;
+      }
+      // we take a buffer of one minute(60 * 1000 ms) before access token expires
+      if (Date.now() < token.expiresAt! * 1000 - 60 * 1000) {
+        return token;
+      } else {
+        try {
+          const response = await requestRefreshOfAccessToken(token);
+
+          const tokens: TokenSet = await response.json();
+
+          if (!response.ok) throw tokens;
+
+          const updatedToken: JWT = {
+            ...token, // Keep the previous token properties
+            idToken: tokens.id_token,
+            accessToken: tokens.access_token,
+            expiresAt: Math.floor(
+              Date.now() / 1000 + (tokens.expires_in as number)
+            ),
+            refreshToken: tokens.refresh_token ?? token.refreshToken,
+          };
+          return updatedToken;
+        } catch (error) {
+          console.error("Error refreshing access token", error);
+          return { ...token, error: "RefreshAccessTokenError" };
+        }
+      }
+    },
+    async session({ session, token }) {
+      // add the access token to the session
+      console.log("session", session);
+      console.log("token", token);
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
 };
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
